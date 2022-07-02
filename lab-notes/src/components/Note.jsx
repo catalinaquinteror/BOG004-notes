ac99f0a89e9effcc2feba2ce2a53cb6215877021
Catalina Quintero <ancquinterori@unal.edu.co>
2022-07-01 22:37:46 -0500

    merge

--- lab-notes/src/components/Note.jsx
+++ lab-notes/src/components/Note.jsx
@@ -2,11 +2,11 @@
 import React from "react";
 //import '../styles/Notes.css';
 
-export const NotesPrueba = [
-  { text: "hola" },
-  { text: "hola2" },
-  { text: "hola3" },
-];
+// export const NotesPrueba = [
+//   { text: "hola" },
+//   { text: "hola2" },
+//   { text: "hola3" },
+// ];
 
 export const Note = ({ text }) => {
   return (
